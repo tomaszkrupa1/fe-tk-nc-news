d66a7f705e8259ee25a57c44c69cf9ea9f58e41f
tomaszkrupa1 <tomaszkrupa1999@gmail.com>
2023-01-12 12:50:03 +0000

    Date format change

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,12 @@
+import moment from 'moment'
+
 export const CommentCard =({comment_id, body, votes, author, created_at}) => {
     return (
         <div className='comment-card'>
             <h3>By {author}</h3>
             <h4>{body}</h4>
             <p>Votes: {votes}</p>
-            <p>Created At:{created_at}</p>
+            <p>Created On: {moment({created_at}).format('L')}</p>
         </div>
     )
 }
