d711f04d55deccde1557785b0a7eeb45803afee7
Tomasz K <tomaszkrupa1999@gmail.com>
2023-08-29 13:09:09 +0100

    Style updates

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,11 +3,13 @@ import {Link} from "react-router-dom";
 
 function ArticleCard({article_id, title, topic, author, body, created_at, votes, comment_count}) {
     return (
-        <Link to={`/articles/${article_id}`}>
+        <Link to={`/articles/${article_id}`} style={{ textDecoration: 'none' }}>
       <article className="article-card">
-        <h2>{title}</h2>
-        <h3>By {author} in {topic}</h3>
-        <h4>{body}</h4>
+        <div className="article-header">
+        <h2 className="article-title">{title}</h2>
+        <h3 className="article-by">By {author} in {topic}</h3>
+        </div>
+        <h4 className="article-body">{body.substring(0, 400)}...</h4>
         <div className="article-stats">
         <p>Votes: {votes} Comments: {comment_count}</p>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-  <Link to="/" >
-    <div className="header">
+  <Link to="/" style={{ textDecoration: 'none' }}>
+    <div className="header" >
   SEENIT
 </div>
   </Link>
