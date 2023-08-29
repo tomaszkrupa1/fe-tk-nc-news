3664f7da3e40b8d7ef8d6a2eddd40408fd82c3c4
Tomasz K <tomaszkrupa1999@gmail.com>
2023-08-29 13:29:37 +0100

    Fixed post creation date

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,8 +3,8 @@ import moment from 'moment'
 export const CommentCard =({comment_id, body, votes, author, created_at}) => {
     return (
         <div className='comment-card'>
-            <h3>By {author}</h3>
-            <h4>{body}</h4>
+            <h3>{author}</h3>
+            <p>{body}</p>
             <p>Votes: {votes}</p>
             <p>Created On: {moment({created_at}).format('L')}</p>
         </div>
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
 import { getArticlesById } from '../api';
 import { CommentList } from './CommentLIst';
+import moment from 'moment'
 
 
-export const SingleArticle = () => {
+export const SingleArticle = (created_at) => {
     const [singleArticle, setSingleArticle] = useState({});
     const {articleId} = useParams();
 
@@ -22,7 +23,7 @@ export const SingleArticle = () => {
             <h3 className="article-by">By {singleArticle.author} in {singleArticle.topic}</h3>
             </div>
             <h4>{singleArticle.body}</h4>
-            <p>Created At: {singleArticle.created_at}</p>
+            <p>Created On: {moment({created_at}).format('L')}</p>
             <div className="article-stats">
             <p>Votes: {singleArticle.votes}</p>
             <p>Comment Count: {singleArticle.comment_count}</p>
